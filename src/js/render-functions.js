c834b1fc9c26bf3e26692167368d102bc4531074
Evelina <evelina20082707@gmail.com>
2026-03-27 12:34:44 +0200

    edit render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -20,6 +20,13 @@ export function createGallery(images) {
             loading="lazy"
           />
         </a>
+
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${img.likes}</p>
+          <p class="info-item"><b>Views</b> ${img.views}</p>
+          <p class="info-item"><b>Comments</b> ${img.comments}</p>
+          <p class="info-item"><b>Downloads</b> ${img.downloads}</p>
+        </div>
       </li>
     `
     )
